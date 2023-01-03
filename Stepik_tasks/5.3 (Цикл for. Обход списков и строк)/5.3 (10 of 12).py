d26ead1d074c4617dae62b54e0f86d8d4f5bfd8a
raskote <raskote@gmail.com>
2023-01-03 15:54:30 +0300

    updated 5.2 all

---
+++ Stepik_tasks/5.3 (Цикл for. Обход списков и строк)/5.3 (10 of 12).py
@@ -0,0 +1,4 @@
+# Задача:
+# https://i.gyazo.com/fbce0857ab086f59186276dbc90b22d0.png
+
+# Решение:
--- Stepik_tasks/5.3 (Цикл for. Обход списков и строк)/5.3 (9 of 12).py
+++ Stepik_tasks/5.3 (Цикл for. Обход списков и строк)/5.3 (9 of 12).py
@@ -2,6 +2,9 @@
 # https://i.gyazo.com/afbe65cd69b983786757cc951bd5aaf3.png
 
 # Решение:
-
-
-# Альтернатива_1 (mine):
+n = list(map(str, input().lower()))
+print(n)
+x = []
+for i in n:
+    x.append(n.count(i))
+print(max(x))
