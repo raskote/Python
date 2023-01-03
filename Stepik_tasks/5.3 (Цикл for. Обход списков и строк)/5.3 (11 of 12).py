cb1dc5ec48025e7eae1a657d8ed91d01007c7869
raskote <raskote@gmail.com>
2023-01-04 02:34:01 +0300

    updated 5.2 all

--- Stepik_tasks/5.3 (Цикл for. Обход списков и строк)/5.3 (11 of 12).py
+++ Stepik_tasks/5.3 (Цикл for. Обход списков и строк)/5.3 (11 of 12).py
@@ -2,6 +2,22 @@
 # https://i.gyazo.com/a8af4e5bcbe1e2d2c46ab2991cbde805.png
 
 # Решение:
+n = input()
+count = 0
+sum = 0
+for i in range(1, len(n)):
+    if n[i].isdigit():
+        count += 1
+        sum += int(n[i])
+print(count, sum)
 
+# Альтернатива_1 (not mine)
+a = [int(i) for i in input() if i.isdigit()]
+print(len(a), sum(a))
 
-# Альтернатива (not mine)
+# Альтернатива_2 (not mine)
+s, n = list(input()), []
+for i in s:
+    if i.isdigit():
+         n.append(int(i))
+print(len(n), sum(n))
