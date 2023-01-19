814713bfde2b724d26a9ac190d365c72c688642e
raskote <raskote@gmail.com>
2023-01-19 21:22:35 +0300

    added 5.6 (2 of 7)

--- Stepik_tasks/5.6 (Вложенные циклы)/5.6 (7 of 7).py
+++ Stepik_tasks/5.6 (Вложенные циклы)/5.6 (7 of 7).py
@@ -2,3 +2,13 @@
 # https://i.gyazo.com/e2d8d346d3c4a02f03a2e4d7df6ce112.png
 
 # Решение:
+n = int(input())
+l = list(map(int, input().split()))
+for i in range(2, n):
+    x = l[i]
+    j = i
+    while j >=1 and l[j-1]>x:
+        l[j] = l[j-1]
+        j = j-1
+    l[j] = x
+print(*l)
