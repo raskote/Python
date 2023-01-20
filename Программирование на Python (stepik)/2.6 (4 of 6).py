b6758857050aca30a7b3e42daa8bd39dc6ba7f1e
raskote <raskote@gmail.com>
2023-01-20 21:13:04 +0300

    updated

--- Программирование на Python (stepik)/2.6 (4 of 6).py
+++ Программирование на Python (stepik)/2.6 (4 of 6).py
@@ -2,3 +2,23 @@
 # https://i.gyazo.com/dfbda78d09e521cef74dbe5d61bc2037.png
 
 # Решение:
+c = [int(y) for y in input().split()]
+x = int(input())
+a = []
+for i in range(len(c)):
+    if c[i] == x:
+        a.append(i)
+if len(a) == 0:
+    print("Отсутствует")
+else:
+    print(*a)
+
+# Альтернатива (not mine)
+# lst = [int(i) for i in input().split()]
+# x = int(input())
+# for i in range(len(lst)):
+#     if
+# lst[i] == x:
+# print(i, end=" ")
+# if x not in lst:
+#     print('Отсутствует')
---
+++ Программирование на Python (stepik)/2.6 (5 of 6).py
@@ -0,0 +1,4 @@
+# Задача:
+# https://i.gyazo.com/bb307c095f42df23d8a682a96a17831c.png
+
+# Решение:
