5aa7c57a02ac379c0a17086dcdb38331886a0b69
raskote <raskote@gmail.com>
2023-01-30 21:04:12 +0300

    updated

---
+++ Поколение Python. Курс для начинающих (stepik)/2.5 Целочисленная арифметика. Часть 2/2.5 (5 of 9).py
@@ -0,0 +1,5 @@
+# Задача:
+# https://i.gyazo.com/a385e849e9bfb9902c5687e51e07d1e2.png
+
+# Решение:
+print(-1 * int(input())//4 * -1)
--- Программирование на Python (stepik)/2.6 (5 of 6).py
+++ Программирование на Python (stepik)/2.6 (5 of 6).py
@@ -2,3 +2,16 @@
 # https://i.gyazo.com/bb307c095f42df23d8a682a96a17831c.png
 
 # Решение:
+b = []
+n = []
+while True:
+    a = input()
+    if a == "end":
+        break
+    else:
+        b.append([int(x) for x in a.split()])
+        continue
+for i in b:
+    # for j in b:
+    print(b)
+        # print(b[i][j])
