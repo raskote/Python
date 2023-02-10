4b09934c02f0b8dded7b84e71796066951991199
raskote <raskote@gmail.com>
2023-02-11 00:23:44 +0300

    updated

--- Поколение Python. Курс для начинающих (stepik)/7.9 (6 of 7).py
+++ Поколение Python. Курс для начинающих (stepik)/7.9 (6 of 7).py
@@ -1,11 +1,5 @@
 # Задача:
-# https://i.gyazo.com/425363301364e519b5fa478131c54c1b.png
+# https://i.gyazo.com/e7937da830703c933e4a938c439cad82.png
 
 # Решение:
 
-n = "734573659783465783465978346593487"
-sum = 0
-for i in n:
-    sum += int(i)
-    print(i)
-print(sum)
