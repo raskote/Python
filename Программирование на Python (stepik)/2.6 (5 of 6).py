182112ed0bd918fceb96e6ad25b135831abd1a23
raskote <raskote@gmail.com>
2023-03-15 23:33:32 +0300

    updated

--- Программирование на Python (stepik)/2.6 (5 of 6).py
+++ Программирование на Python (stepik)/2.6 (5 of 6).py
@@ -3,15 +3,18 @@
 
 # Решение:
 b = []
-n = []
+c = []
 while True:
     a = input()
     if a == "end":
         break
     else:
         b.append([int(x) for x in a.split()])
+        c.append([int(x) for x in a.split()])
         continue
-for i in b:
-    # for j in b:
-    print(b)
-        # print(b[i][j])
+for i in range(len(b)):
+    for j in range(len(b[i])):
+        c[i][j] = (b[i-1][j] + b[i-len(b)+1][j] + b[i][j-1] + b[i][j-len(b[i])+1])
+
+for k in range(len(c)):
+    print(*c[k])
---
+++ Программирование на Python (stepik)/2.6 (6 of 6).py
@@ -0,0 +1,9 @@
+# Задача:
+# https://i.gyazo.com/712298b1ce4388d4ac7d0a87ad96c37b.png
+
+# Решение:
+n = int(input())
+for i in range(1, n):
+    print(i, end="\n")
+    for j in range(1, n):
+        print(j, end= " ")
