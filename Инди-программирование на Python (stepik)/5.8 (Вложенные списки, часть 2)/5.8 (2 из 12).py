c4b7afeea8cf98d2aa61ad6d9b7014b7acdefdbf
raskote <raskote@gmail.com>
2023-04-12 12:20:49 +0300

    updated

--- Инди-программирование на Python (stepik)/5.8 (Вложенные списки, часть 2)/5.8 (2 из 12).py
+++ Инди-программирование на Python (stepik)/5.8 (Вложенные списки, часть 2)/5.8 (2 из 12).py
@@ -2,3 +2,18 @@
 # https://i.gyazo.com/f9ce6d5136fd9288637548c476d4b453.png
 
 # Решение:
+n = int(input())
+a,b,c = map(int, input().split())
+li = []
+for i in range(n):
+    row = []
+    for j in range(n):
+        row.append(a)
+    li.append(row)
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            li[i][j] = c
+        if i > j:
+            li[i][j] = b
+    print(*li[i])
