de334567f7acd25ffcf05fcd842802cf6a48f799
raskote <raskote@gmail.com>
2023-04-29 23:15:29 +0300

    updated

--- Инди-программирование на Python (stepik)/5.8 (Вложенные списки, часть 2)/5.8 (8 из 10).py
+++ Инди-программирование на Python (stepik)/5.8 (Вложенные списки, часть 2)/5.8 (8 из 10).py
@@ -2,3 +2,14 @@
 # https://i.gyazo.com/7a9c5f0dccb1751c6fce4cecdb79378b.png
 
 # Решение:
+n, m = map(int, input().split())
+li = [input() for i in range(n)]
+flag = False
+for i in range(n):
+    for j in range(m):
+        if li[i][j] in ['C', 'M', "Y"]:
+            flag = True
+if flag:
+    print("#Color")
+else:
+    print("#Black&White")
---
+++ Инди-программирование на Python (stepik)/5.8 (Вложенные списки, часть 2)/5.8 (9 из 10).py
@@ -0,0 +1,5 @@
+# Задача:
+# https://i.gyazo.com/8fc7cb9a3d51e70785800ca2748ff0a9.png
+
+# Решение:
+n = int(input())
