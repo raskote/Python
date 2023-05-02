1caed2d91446bd5f85ee85ecd97f8ef2434ceef5
raskote <raskote@gmail.com>
2023-05-03 00:40:53 +0300

    updated

---
+++ Инди-программирование на Python (stepik)/5.9 (Генераторы списков)/5.9 (1 из 9).py
@@ -0,0 +1,7 @@
+# Задача:
+# https://i.gyazo.com/4578bf530b1e35fe8805c46145bd65d7.png
+
+# Решение:
+zeroes = [0 for i in range(100)]
+print(zeroes)
+print(len(zeroes))
---
+++ Инди-программирование на Python (stepik)/5.9 (Генераторы списков)/5.9 (2 из 9).py
@@ -0,0 +1,6 @@
+# Задача:
+# https://i.gyazo.com/1264c729f70362e5dd6d64e1a14d8577.png
+
+# Решение:
+li = [i for i in range(1, int(input())+1)]
+print(li)
