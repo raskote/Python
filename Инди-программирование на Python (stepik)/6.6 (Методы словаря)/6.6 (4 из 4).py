b7ba815e6a7837f6ff40e50031726c3e95f6d456
raskote <raskote@gmail.com>
2023-05-18 01:44:27 +0300

    updated

--- Инди-программирование на Python (stepik)/6.6 (Методы словаря)/6.6 (4 из 4).py
+++ Инди-программирование на Python (stepik)/6.6 (Методы словаря)/6.6 (4 из 4).py
@@ -2,5 +2,11 @@
 # https://i.gyazo.com/c34a5a2309dc280f830d5b2a5ad12443.png
 
 # Решение:
-d = list(map(int, input().split()))
-print(d)
+l = list(map(int, input().split()))
+li = [{} for i in range(len(l))]
+n = len(l)-2
+li[-1].setdefault(l[-2], l[-1])
+while n > 0:
+    li[n].setdefault(l[n-1], li[n+1])
+    n -= 1
+print(li[1])
