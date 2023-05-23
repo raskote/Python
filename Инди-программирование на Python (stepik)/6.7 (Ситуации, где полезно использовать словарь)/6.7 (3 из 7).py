484c899924cfe900bff528412a4376021fd44d77
raskote <raskote@gmail.com>
2023-05-23 23:47:36 +0300

    updated

--- Инди-программирование на Python (stepik)/6.7 (Ситуации, где полезно использовать словарь)/6.7 (3 из 7).py
+++ Инди-программирование на Python (stepik)/6.7 (Ситуации, где полезно использовать словарь)/6.7 (3 из 7).py
@@ -2,3 +2,19 @@
 # https://i.gyazo.com/825661e1fbded0348d2d2302c0e8a078.png
 
 # Решение:
+supermarket = {
+    "milk": {"quantity": 20, "price": 1.19},
+    "biscuits": {"quantity": 32, "price": 1.45},
+    "butter": {"quantity": 20, "price": 2.29},
+    "cheese": {"quantity": 15, "price": 1.90},
+    "bread": {"quantity": 15, "price": 2.59},
+    "cookies": {"quantity": 20, "price": 4.99},
+    "yogurt": {"quantity": 18, "price": 3.65},
+    "apples": {"quantity": 35, "price": 3.15},
+    "oranges": {"quantity": 40, "price": 0.99},
+    "bananas": {"quantity": 23, "price": 1.29}
+}
+sum = 0
+for i in supermarket:
+    sum += supermarket[i]["quantity"]*supermarket[i]["price"]
+print(sum)
