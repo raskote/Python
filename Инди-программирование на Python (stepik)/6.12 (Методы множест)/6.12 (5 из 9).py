85cd225c75fb0507fc8f7ce2d995123610e750ad
raskote <raskote@gmail.com>
2023-06-01 01:55:29 +0300

    updated

--- Инди-программирование на Python (stepik)/6.12 (Методы множест)/6.12 (5 из 9).py
+++ Инди-программирование на Python (stepik)/6.12 (Методы множест)/6.12 (5 из 9).py
@@ -2,3 +2,14 @@
 # https://i.gyazo.com/daf97de0f3cfdbc44e4e2a54f0f47b27.png
 
 # Решение:
+x = input().lower().split(',')
+y = set()
+for i in x:
+    if i not in y:
+        y.add(i)
+        print("НЕТ")
+    else:
+        print("ДА")
+
+
+
