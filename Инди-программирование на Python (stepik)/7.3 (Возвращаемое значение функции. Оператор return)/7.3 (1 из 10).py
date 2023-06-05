8c9c0a3d053f899d38e25ef1ab79fe82fbaff060
raskote <raskote@gmail.com>
2023-06-06 00:19:39 +0300

    updated

---
+++ Инди-программирование на Python (stepik)/7.3 (Возвращаемое значение функции. Оператор return)/7.3 (1 из 10).py
@@ -0,0 +1,7 @@
+# Задание:
+# https://i.gyazo.com/fb3e3b3a6d50de968a639eaa4a32f215.png
+
+# Решение:
+def is_person_teenager(age):
+    return 12 <= age <= 17
+
--- Инди-программирование на Python (stepik)/7.3 (Возвращаемое значение функции. Оператор return)/7.3 (1 из 11).py
+++
@@ -1,4 +0,0 @@
-# Задание:
-# https://i.gyazo.com/4db593f427f14841130aafb7e4190d5b.png
-
-# Решение:
