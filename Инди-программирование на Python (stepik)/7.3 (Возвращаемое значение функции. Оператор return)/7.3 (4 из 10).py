9c30289021c7d4702caa51972bed83b43ec4e49f
raskote <raskote@gmail.com>
2023-06-07 23:44:23 +0300

    updated

--- Инди-программирование на Python (stepik)/7.3 (Возвращаемое значение функции. Оператор return)/7.3 (4 из 10).py
+++ Инди-программирование на Python (stepik)/7.3 (Возвращаемое значение функции. Оператор return)/7.3 (4 из 10).py
@@ -2,3 +2,16 @@
 # https://i.gyazo.com/4fa4d0dc9c36325bee2701a1c08dc75e.png
 
 # Решение:
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+
+n = int(input())
+li = []
+li2 = []
+for i in range(n):
+    li.append(int(input()))
+for i in range(1, len(li)):
+    li2.append(gcd((sorted(li))[0], li[i]))
+print(min(li2))
