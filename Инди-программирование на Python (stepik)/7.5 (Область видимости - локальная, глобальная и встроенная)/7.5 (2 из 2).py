55068a8e4921d093f52cfbacc1bbb9e3cf3df3e0
raskote <raskote@gmail.com>
2023-06-15 00:49:21 +0300

    updated

--- Инди-программирование на Python (stepik)/7.5 (Область видимости - локальная, глобальная и встроенная)/7.5 (2 из 2).py
+++ Инди-программирование на Python (stepik)/7.5 (Область видимости - локальная, глобальная и встроенная)/7.5 (2 из 2).py
@@ -3,7 +3,25 @@
 
 # Решение:
 def get_word_indices(strings: list[str]) -> dict:
+    strings = [i.lower() for i in strings]
+    li_full = [i.split() for i in strings]
+    li_small = []
+    for i in strings:
+        li_small.extend(i.split())
+    di = {}
+    set_li = set(li_small)
+    for i in set_li:
+        new_li = []
+        for j in range(len(li_full)):
+            if i in li_full[j]:
+                new_li.append(j)
+        di.setdefault(i, new_li)
+    return di
 
-
-
-
+# Альтернатива (not_mine):
+# def get_word_indices(strings: list[str]) -> dict:
+#     d = {}
+#     for i, k in enumerate(strings):
+#         for j in k.lower().split():
+#             d.setdefault(j, []).append(i)
+#     return d
